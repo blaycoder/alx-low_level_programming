08c134f75d3a51a67c14c12bd9872ca2d6131b13
blaycoder <onatola4real@gmail.com>
2022-03-10 09:44:35 -0800

    modify a file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,17 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
 
-int main(void){
-  
-  int intType;
-  char charType;
-  float floatType;
-  long int long_int_Type;
-  long long int long_long_int_Type;
-  
-  printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-  printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-  printf("Size of a long int: %zu byte(s)\n", sizeof(long_int_Type));
-  printf("Size of a long long int: %zu byte(s)\n", sizeof(long_long_int_Type));
-  printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-
-  return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
 }
+
