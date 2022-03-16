7f885ae6add7af2bce3f743b8b8a2dfa15ec09cb
blaycoder <onatola4real@gmail.com>
2022-03-16 14:06:42 -0700

    addd a new file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_sign - print +, 0, or - according to the number
+ *@n: the letter to check
+ *
+ * Return: O if it's zero, 1 if is possitive, -1 is negative
+ * On error
+ */
+
+int print_sign(int n)
+{
+if (n > 48)
+{
+_putchar(43);
+return (1);
+}
+else if (n == 48)
+{
+_putchar(48);
+return (0);
+}
+else if (n < 48)
+{
+_putchar(45);
+} return (-1);
+}
