4410b160318bfc1b9d287919f42b7c625c0dd11d
blaycoder <onatola4real@gmail.com>
2022-03-16 14:22:41 -0700

    added a new file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs - prints absolue vale of an integer
+ *@int: the letter to check
+ *
+ * Return: O if it's zero, 1 if is possitive, -1 is negative
+ * On error
+ */
+
+int _abs(int)
+{
+_putchar(int);
+return (0);
+}
