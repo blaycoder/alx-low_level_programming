0422a18a2d793bfb274dc8cb62117b6a7d628274
blaycoder <onatola4real@gmail.com>
2022-03-16 17:28:32 -0700

    added new file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,17 +2,18 @@
 
 /**
  * add - add two integers and return the result
- *@sum: result of values sum
+ *@s: result of values sum
  *@a:  Value 1
- *b: Value 2
+ *@b: Value 2
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
- */
+*/
 
 int add(int a, int b)
 {
-int sum;
-sum = (a + b);
-return (sum);
+	int r;
+
+		r = (a + b);
+	return (r);
 }
