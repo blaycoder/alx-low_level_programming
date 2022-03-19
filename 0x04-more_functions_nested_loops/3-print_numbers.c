d5f494a3f07c4c706e56e1bac90b78a3d1f33fe2
blaycoder <onatola4real@gmail.com>
2022-03-18 17:49:49 -0700

    upload file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print a sequence from 0 to 9
+ *
+ * automated sequence to variable needed
+ * Return: no returns
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar('\n');
+}
