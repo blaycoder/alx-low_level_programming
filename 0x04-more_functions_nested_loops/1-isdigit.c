6007d7bd97495ec6587370cc9b880d35d9b219f6
blaycoder <onatola4real@gmail.com>
2022-03-18 17:58:47 -0700

    made changes on fil

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,14 +7,11 @@
  * Return: On success 1.
  * On error or no uppercase, 0 is returned.
  */
-
 int _isdigit(int c)
 {
-if (c >= 48 && c <= 57)
-{
-return (0);
-}
-else
-{
-return (0);
+
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
 }
