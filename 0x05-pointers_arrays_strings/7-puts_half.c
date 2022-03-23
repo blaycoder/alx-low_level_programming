7ac148138a41911a12ffbe4ad62ee65bae61effe
blaycoder <onatola4real@gmail.com>
2022-03-22 18:50:21 -0700

    added a new file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int counter;
+	int half_counter;
+
+	for (counter = 0; str[counter] != '\0'; counter++)
+	{
+	}
+	if (counter % 2 == 0)
+		half_counter = counter / 2;
+	else
+		half_counter = ((counter + 1) / 2);
+	counter--;
+	for (; half_counter <= counter; half_counter++)
+	{
+		_putchar(*(str + half_counter));
+	}
+	half_counter--;
+	_putchar('\n');
+}
