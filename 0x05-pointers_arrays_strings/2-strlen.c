022a977f8f5c9b58b2745d6f3c7fbd0c99dce162
blaycoder <onatola4real@gmail.com>
2022-03-22 17:52:09 -0700

    added a new file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns length of a string
+ *@s: string parameter
+ * Return: Always 1.
+ */
+
+int _strlen(char *s)
+{
+_strlen(*s);
+return (1);
+}
