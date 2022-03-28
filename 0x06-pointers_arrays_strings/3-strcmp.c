f7af6f82b5ff889b71033471ed8c2d742e226838
blaycoder <onatola4real@gmail.com>
2022-03-28 15:04:43 -0700

    made changes on a file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,13 +8,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int value = _strcmp(s1, s2);
-int i = 0;
-int j = 0;
-if (value == 0)
+while (*s1 == *s2)
+{
+if (*s1 != '\0')
+{
 return (0);
-else if (value > 0)
-return (1);
-else if (value < 0)
-return (-1);
+}
+s1++;
+s2++;
+}
+return (*s1 - *s2)
 }
