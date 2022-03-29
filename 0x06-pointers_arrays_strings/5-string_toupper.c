01aec7c67c99867da577bb726d9e67e2bfa66bb8
blaycoder <onatola4real@gmail.com>
2022-03-28 18:20:30 -0700

    new file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* string_toupper -function to change lowercase letters of a string to uppercase
+* @s: string to converts
+*Return: return 0
+*/
+
+char *string_toupper(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+s[i] = s[i] - 32;
+}
+}
+return (s);
+}
