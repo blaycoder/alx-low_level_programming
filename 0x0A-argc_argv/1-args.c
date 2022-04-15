2d1b31fab968dbcea45bba4ef6a0b42da82a5a6d
blaycoder <onatola4real@gmail.com>
2022-04-15 02:54:28 -0700

    made changes

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,8 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc, char* argv[])
+int main(int argc, char* argv[] __attribute__((unused)))
 {
 printf("%d\n", argc-1);
-(void)argv;
 return (0);
 }
